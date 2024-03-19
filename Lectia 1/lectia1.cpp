8fe24423f333b8c11b0d7604d995e739652bbaf2
Phil999 <72974980+Phi999@users.noreply.github.com>
2024-03-19 17:19:56 +0200

    Update lectia1.cpp

--- Lectia 1/lectia1.cpp
+++ Lectia 1/lectia1.cpp
@@ -1,1 +1,16 @@
+// C++ code
+//
+int beculet = 10;
 
+void setup()
+{
+  pinMode(beculet, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(beculet, HIGH);
+  delay(1000); //asteapta 1 secunda
+  digitalWrite(beculet, LOW);
+  delay(1000); // asteapta 1 secunda
+}
